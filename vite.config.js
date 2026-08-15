d5ec960ce1ffa8ac3481433965f60a7d996ba6e3
LZShahaha <lzs914371843@gmail.com>
2026-08-15 14:48:59 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs me/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: '变好看日志 App',
-        short_name: '变好看',
-        description: '变好看日志记录',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone', // 🔑 核心：强制全屏无地址栏
-        icons: [
-          {
-            src: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
-  base: './' // 确保路径正确
+  plugins: [react()],
+  base: './'
 })
